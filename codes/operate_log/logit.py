4fca4078d36f18ddcc09ac0af76a36f3485f88fe
mayao@windeal.onaliyun.com <mayao@windeal.onaliyun.com>
2022-08-30 11:27:39 +0800

    代码补全

--- codes/operate_log/logit.py
+++ codes/operate_log/logit.py
@@ -1,6 +1,10 @@
 import inspect
 from inspect import isfunction
 import types
+
+""" 
+ref: https://www.zhihu.com/question/26848331
+"""
 class Person:
 
     def __init__(self,name,age) -> None:
@@ -19,7 +23,7 @@ class Tool:
 
 def diff_log(old_inst,new_inst):
     """
-    比较两个任意对象的属性不同
+    比较两个任意对象的新老属性不同
     """
     def class_name_of_inst(_inst):
         return type(_inst).__name__
@@ -28,11 +32,17 @@ def diff_log(old_inst,new_inst):
     if class_of_new_inst == class_of_old_inst:
         old_values = get_attr_of_obj(old_inst)
         new_values = get_attr_of_obj(new_inst)
-        for attr in old_inst:
-            pass
-            
+        all_values =set(old_values + new_values) 
+        diff_map = dict()
+        for attr in all_values:
+            print(attr)
+            old_value = getattr(old_inst,attr)
+            new_value = getattr(new_inst,attr)
+            if old_value != new_value:
+                item = f'{attr} of {class_of_new_inst} from {old_value} to {new_value}'
+                diff_map[attr] = item
+    return diff_map
 
-alean = Person('alean',28)
 
 def get_attr_of_obj(inst):
     attributes = dir(inst)
@@ -40,9 +50,11 @@ def get_attr_of_obj(inst):
     b = [a for a in attributes if not(a.startswith('__') and a.endswith('__'))]
     for item in b:
         if not isinstance(getattr(inst,item),types.MethodType):
-            ret.append(getattr(inst,item))
+            ret.append(item)
 
     return ret
-
-ret = get_attr_of_obj(alean)
+alean = Person('alean',28)
+peter = Person('peter',None)
+ret=diff_log(alean,peter)
+# ret = get_attr_of_obj(alean)
 print(ret)
